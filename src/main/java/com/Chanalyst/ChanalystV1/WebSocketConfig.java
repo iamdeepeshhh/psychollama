3bf6082be4ef987f66468fd631e08ad38562c4cf
Deepak Gangwani <deepakhofc@gmail.com>
2025-09-25 23:01:20 +0530

    d new changes for testing 5

--- src/main/java/com/Chanalyst/ChanalystV1/WebSocketConfig.java
+++ src/main/java/com/Chanalyst/ChanalystV1/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.Chanalyst.ChanalystV1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,6 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Bean
+    public ThreadPoolTaskScheduler messageBrokerTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        scheduler.initialize();
+        return scheduler;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -19,7 +30,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic")
-                .setHeartbeatValue(new long[]{10000, 10000}); // server → clients
+                .setHeartbeatValue(new long[]{10000, 10000}) // server → clients
+                .setTaskScheduler(messageBrokerTaskScheduler());
         registry.setApplicationDestinationPrefixes("/app"); // client → server
     }
 }
